e2ded6a795361c112638af80052a85d143616585
jhomc <jhomcosta@gmail.com>
2022-12-30 20:11:01 -0300

    removidos comentarios

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
   const { username } = request.headers;
 
   const user = users.find((user) => user.username === username);
@@ -25,7 +24,6 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 app.post("/users", (request, response) => {
-  // Complete aqui
   const { name, username } = request.body;
 
   const userExists = users.some((user) => user.username === username);
@@ -47,14 +45,12 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
   const { user } = request;
 
   return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
   const { title, deadline } = request.body;
   const { user } = request;
 
@@ -72,7 +68,6 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
   const { title, deadline } = request.body;
   const { user } = request;
   const { id } = request.params;
@@ -90,7 +85,6 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
   const { user } = request;
   const { id } = request.params;
 
@@ -106,7 +100,6 @@ app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
   const { user } = request;
   const { id } = request.params;
 
